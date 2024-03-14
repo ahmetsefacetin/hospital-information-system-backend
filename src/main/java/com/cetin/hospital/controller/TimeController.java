fb5e51eb7d9439ccf6190ffa63f7621b9f16a1e6
ahmetsefacetin <ahmet80sefa@hotmail.com>
2024-03-14 05:56:33 +0300

    .

--- src/main/java/com/cetin/hospital/controller/TimeController.java
+++ src/main/java/com/cetin/hospital/controller/TimeController.java
@@ -1,14 +1,15 @@
 package com.cetin.hospital.controller;
 
+import com.cetin.hospital.service.TimeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/times")
 public class TimeController {
-    private final TimeController timeController;
+    private final TimeService timeService;
 
-    public TimeController(TimeController timeController) {
-        this.timeController = timeController;
+    public TimeController(TimeService timeService) {
+        this.timeService = timeService;
     }
 }
